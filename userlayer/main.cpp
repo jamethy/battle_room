ffa638aa883094cecc70d5ad9e8a1878189a8117
jamethy <jamesianburns@gmail.com>
2015-08-30 22:59:11 -0400

    Modified main to actually work.

--- userlayer/main.cpp
+++ userlayer/main.cpp
@@ -1,12 +1,14 @@
 #include "interfaces/graphics_interface.h"
 #include "interfaces/gamedata_interface.h"
+#include <iostream>
 #include <chrono>
 #include <thread>
 
-double ng_time = 0;
+using namespace std::chrono;
+
+ duration<double> ng_time;
 unsigned ng_count = 0;
 
-using std::chrono::steady_clock;
 
 void updateGame(GameInterface::GameDataClass* gd){ gd->update(); }
 
@@ -24,7 +26,7 @@ void updateNongraphics(
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
     steady_clock::time_point stop = steady_clock::now();
-    ng_time += std::chrono::duration_cast<double> (stop-start);
+    ng_time += duration_cast< duration<double> > (stop-start);
     ++ng_count;
 }
 
@@ -33,7 +35,7 @@ int main()
     GraphicsInterface::UniqueGraphicsWindow w = GraphicsInterface::createWindow(500,500);
     GameInterface::UniqueGameData gd = GameInterface::createGameData();
 
-    double g_time = 0;
+    duration<double> g_time;
     unsigned g_count = 0;
 
     for(unsigned i = 0; i < 120; ++i){
@@ -43,14 +45,14 @@ int main()
         std::thread nongraphics(updateNongraphics,gd.get());
         w->update();
         nongraphics.join();
-        steady_clock::time_point = stop = steady_clock::now();
-        ng_time += std::chrono::duration_cast<double> (stop-start);
-        ++ng_count;
+        steady_clock::time_point stop = steady_clock::now();
+        g_time += duration_cast< duration<double> > (stop-start);
+        ++g_count;
         std::this_thread::sleep_until(start + std::chrono::milliseconds(17)); // temporary max fps of 60
     }
 
-    std::cout << ng_time/ng_count << std::endl;
-    std::cout << g_time/g_count << std::endl;
+    std::cout << ng_time.count()/ng_count << std::endl;
+    std::cout << g_time.count()/g_count << std::endl;
 
 	return 0;
 }
